d4a7affaae9f0d64181d1de9a9738f08f0f8af9f
lao8n <hellolao8n@gmail.com>
2019-02-09 02:07:42 +0000

    Update pom.xml to run App.java with javafx

--- src/main/java/groupb/battlestar/App.java
+++ src/main/java/groupb/battlestar/App.java
@@ -4,10 +4,21 @@ package groupb.battlestar;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+
+public class App extends Application {
+  public void start(Stage stage) {
+    Label hello = new Label("Hello JavaFX world!!");
+    Group root = new Group(hello);
+    Scene scene = new Scene(root);
+    stage.setTitle("Hello World!");
+    stage.setScene(scene);
+    stage.show();
+  }
+  public static void main(String[] args) {
+    launch();
+}
 }
