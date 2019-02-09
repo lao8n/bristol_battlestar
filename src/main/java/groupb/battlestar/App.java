7cac008eef655707fe13f0e1d3fe54efcf137e8c
lao8n <hellolao8n@gmail.com>
2019-02-09 00:41:09 +0000

    Create maven project

---
+++ src/main/java/groupb/battlestar/App.java
@@ -0,0 +1,13 @@
+package groupb.battlestar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
