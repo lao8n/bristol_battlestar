d560c466e8e60271fe007b3ca30f31630037e032
Tim van Ellemeet <timvanellemeet@gmail.com>
2019-03-12 17:15:40 +0000

    beautify

--- src/main/java/swarm_wars_library/engine/Entity.java
+++ src/main/java/swarm_wars_library/engine/Entity.java
@@ -3,81 +3,81 @@ package swarm_wars_library.engine;
 
 import javax.swing.text.Position;
 
-import processing.core.PApplet; 
+import processing.core.PApplet;
 
 public class Entity {
 
-  private PApplet sketch; 
+  private PApplet sketch;
   private Tag tag;
-  private Transform transform; 
+  private Transform transform;
   private Render render;
   public Input input;
   private int points;
   private Shooter shooter;
   private Health health;
-  private RigidBody rb; 
+  private RigidBody rb;
   //BOT Specific comps
-  private CommsChannel commsChannel; 
-  private CommsPacket commsPacket; 
+  private CommsChannel commsChannel;
+  private CommsPacket commsPacket;
   //private State state;
-  private SwarmLogic swarmLogic; 
-  private boolean hasRender, hasInput, hasShooter, hasHealth, hasComms, isBot, hasRb, isMothership; 
-  
-  //Entity(tag, scale, hasRender, hasInput, hasShooter, hasHealth, hasComms, hasRb)
-  public Entity(PApplet sketch, Tag t, int sc, boolean r, boolean i, boolean s, boolean h, boolean coms, boolean rigbod){
+  private SwarmLogic swarmLogic;
+  private boolean hasRender, hasInput, hasShooter, hasHealth, hasComms, isBot, hasRb, isMothership;
 
-    this.sketch = sketch; 
-    tag = t; 
+  //Entity(tag, scale, hasRender, hasInput, hasShooter, hasHealth, hasComms, hasRb)
+  public Entity(PApplet sketch, Tag t, int sc, boolean r, boolean i, boolean s, boolean h, boolean coms, boolean rigbod) {
+
+    this.sketch = sketch;
+    tag = t;
     transform = new Transform();
     hasRender = r;
     hasInput = i;
-    hasShooter = s; 
+    hasShooter = s;
     hasHealth = h;
     hasComms = coms;
-    hasRb = rigbod; 
+    hasRb = rigbod;
     isMothership = false;
 
-    if (tag.equals(Tag.P_BOT) || (tag.equals(Tag.E_BOT))){
-      isBot = true; 
+    if (tag.equals(Tag.P_BOT) || (tag.equals(Tag.E_BOT))) {
+      isBot = true;
       //NOTICE: must call method to init swarmLogic in main loop
     }
-    if (hasRender){
+    if (hasRender) {
       render = new Render(sketch, (int) transform.getScale().getX());
     }
     if (hasInput) {
       input = new Input(sketch);
     }
-    if (hasShooter){
+    if (hasShooter) {
       shooter = new Shooter(sketch, tag);
     }
-    if (hasHealth){
+    if (hasHealth) {
       health = new Health(tag);
     }
-    if (hasRb){
+    if (hasRb) {
       rb = new RigidBody();
     }
 
-    if (tag.equals(Tag.PLAYER) || tag.equals(Tag.ENEMY)){
+    if (tag.equals(Tag.PLAYER) || tag.equals(Tag.ENEMY)) {
       isMothership = true;
-      transform.setScale(30, 30); 
+      transform.setScale(30, 30);
 
-    } else if (tag.equals(Tag.P_BULLET) || tag.equals(Tag.E_BULLET)){
-      transform.setScale(5,5);
+    } else if (tag.equals(Tag.P_BULLET) || tag.equals(Tag.E_BULLET)) {
+      transform.setScale(5, 5);
     }
   }
 
-  public void update(){
+  public void update() {
     //draw it
-    if (hasRender){
+    if (hasRender) {
       render.update(transform.getPosition(), tag, transform.getHeading());
     }
-    
+
     //set position with either Input or AI
-    if (hasInput){
+    if (hasInput) {
       input.update();
       transform.setPosition(input.getLocation());
       transform.setHeading(input.getHeading());
-    } 
+    }
 
     //else if (hasAI) {
     //  ai.update(); 
@@ -86,21 +86,21 @@ public class Entity {
     // if (isMothership){
     //   sendPacket();
     // }
-    
-    if (hasShooter){
-      shooter.shoot(transform.getPosition(), transform.getHeading()); 
+
+    if (hasShooter) {
+      shooter.shoot(transform.getPosition(), transform.getHeading());
       shooter.update();
     }
-    
-    if (hasHealth){
+
+    if (hasHealth) {
       health.update();
     }
 
-    if (hasComms){
+    if (hasComms) {
       sendPacket();
     }
-   
-    if (isBot){
+
+    if (isBot) {
       //update bot based on swarm/bot logic using the swarm component
       swarmLogic.setTransform(transform);
       swarmLogic.update();
@@ -108,59 +108,59 @@ public class Entity {
     }
   }
 
-  public void setVelocity(double x, double y){
-      transform.setVelocity(x, y);
+  public void setVelocity(double x, double y) {
+    transform.setVelocity(x, y);
   }
 
-  public Vector2D getVelocity(){
-      return transform.getVelocity();
+  public Vector2D getVelocity() {
+    return transform.getVelocity();
   }
-  
+
   //set if visible
-  public void setRender(boolean value){
+  public void setRender(boolean value) {
     hasRender = value;
   }
-  
+
   //to check if entity is visible
-  public boolean isRendering(){
-    return hasRender; 
+  public boolean isRendering() {
+    return hasRender;
   }
 
-  public void setHeading(double heading){
-      transform.setHeading(heading);
-  }
-
-  public double getHeading(){
-      return transform.getHeading();
-  }
-  
-  //for use by shooter class to set bullet position & heading
-  public void setPosition(Vector2D position, double heading){
+  public void setHeading(double heading) {
     transform.setHeading(heading);
-    transform.setPosition(position); 
+  }
+
+  public double getHeading() {
+    return transform.getHeading();
+  }
+
+  //for use by shooter class to set bullet position & heading
+  public void setPosition(Vector2D position, double heading) {
+    transform.setHeading(heading);
+    transform.setPosition(position);
     //if (hasAI){
     //  ai.setLocation(pos);
     //}
   }
-  
+
   //for use by shooter to get bullet position
-  public Vector2D getPosition(){
+  public Vector2D getPosition() {
     return transform.getPosition();
   }
-  
-  public void kill(){
+
+  public void kill() {
     hasRender = false;
     hasShooter = false;
   }
-  
-  public boolean isDead(){
-    if (hasHealth){
+
+  public boolean isDead() {
+    if (hasHealth) {
       return health.isDead();
     }
     return false;
   }
-  
-  public void setAlive(){
+
+  public void setAlive() {
     //reset velocity to 0
     //if (hasAI){
     //  ai.reset();
@@ -169,36 +169,36 @@ public class Entity {
     hasShooter = true;
     //add something for health
   }
-  
-  public int getScale(){
+
+  public int getScale() {
     return (int) transform.getScale().getX();
   }
 
-  public void setComms(CommsChannel comms){
+  public void setComms(CommsChannel comms) {
     commsChannel = comms;
     commsPacket = new CommsPacket();
-    if(isBot){
+    if (isBot) {
       swarmLogic.setComms(comms);
     }
     sendPacket();
   }
 
   //BOT methods
-  public void setSwarmLogic(){
+  public void setSwarmLogic() {
     //init swarm logic
     swarmLogic = new SwarmLogic(transform, rb);
   }
 
   //ALLL COMMS
-  public void sendPacket(){
-      //update this logic
-      commsPacket.setLocation(transform.getPosition());
-      commsPacket.setIsAlive(true);
-      if(isMothership){
-        commsChannel.setPacket(commsPacket, 0);  
-      } else {
-        commsChannel.setPacket(commsPacket, swarmLogic.getId());
-      }
+  public void sendPacket() {
+    //update this logic
+    commsPacket.setLocation(transform.getPosition());
+    commsPacket.setIsAlive(true);
+    if (isMothership) {
+      commsChannel.setPacket(commsPacket, 0);
+    } else {
+      commsChannel.setPacket(commsPacket, swarmLogic.getId());
+    }
   }
 
   //will need to get and set state here for FSM
--- src/main/java/swarm_wars_library/engine/Render.java
+++ src/main/java/swarm_wars_library/engine/Render.java
@@ -75,3 +75,76 @@ public class Render {
     this.sketch.ellipse(0,0, this.scale, this.scale); 
   }
 }
+
+
+/* ------ DISPLAY CLASS ------ */
+//class Display {
+//   public void display(GameObject go){
+//drawBackground();
+//      pushMatrix();
+//      rectMode(CENTER);
+//      stroke(0);
+//      translate((float)go.getLocationX(), (float)go.getLocationY());
+//      rotate((float)go.getHeading());
+//      drawObject(go);
+//        popMatrix();
+//   }
+
+//TODO : add tracking, moving background image
+//   private void drawBackground(){
+//        background(25,25,76); //dark blue
+//}
+
+//     private void drawObject(GameObject go){
+//       if (go.getGOTag() == "PLAYER"){
+//         drawShip(go);
+//       } else if (go.getGOTag() == "ENV"){
+//         drawEnv(go);
+
+//will add drawBullet, drawEnemy, drawBase etc
+//     } else {
+//do nothing
+//       }
+//     }
+
+//   private void drawShip(GameObject go){
+//go.setScale(30, 20);
+//   go.setScale(20,10);
+//   if (go.getHasCollision()){
+//      fill(random(0, 255),random(0, 255),random(0, 255));
+//   }
+//shadow
+//   noStroke();
+//   fill(33,11,232, 20);
+//   rect(0, 0, (float)go.getScaleX()*1.75, (float)go.getScaleY()*1.75);
+//   fill(33,11,232, 50);
+//   rect(0, 0, (float)go.getScaleX()*1.5, (float)go.getScaleY()*1.5);
+//   fill(33,11,232, 70);
+//   rect(0, 0, (float)go.getScaleX()*1.2, (float)go.getScaleY()*1.2);
+//wing shadows
+//     fill(33, 11, 232, 70);
+//     rect(-(float)go.getScaleX()/1.75, (float)go.getScaleY()/2.5,
+///         (float)go.getScaleX(), (float)go.getScaleY());
+//     rect(-(float)go.getScaleX()/1.75, -(float)go.getScaleY()/2.5,
+//        (float)go.getScaleX(), (float)go.getScaleY());
+//wings
+// fill(33, 11, 232);
+// rect(-(float)go.getScaleX()/1.5, (float)go.getScaleY()/2,
+//      (float)go.getScaleX(), (float)go.getScaleY());
+// rect(-(float)go.getScaleX()/1.5, -(float)go.getScaleY()/2,
+//      (float)go.getScaleX(), (float)go.getScaleY());
+//ship head
+//     fill(77, 77, 255);
+//     rect(0, 0, (float)go.getScaleX(), (float)go.getScaleY());
+//   }
+
+// private void drawEnv(GameObject go){
+//  noStroke();
+// go.setScale(50, 40);
+//fill(128, 128, 128);
+//if (go.getHasCollision()){
+//    fill(random(0, 255),random(0, 255),random(0, 255));
+//}
+//rect(0, 0, (float)go.getScaleX(), (float)go.getScaleY());
+// }
+//}
