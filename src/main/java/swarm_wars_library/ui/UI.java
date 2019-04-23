21bf62f95cfed44e4c9fca3645384e12e80534bc
S. Robinson <scrobinson34@gmail.com>
2019-04-23 10:47:33 +0200

    addition of event handling in main Swarm java file

--- src/main/java/swarm_wars_library/ui/UI.java
+++ src/main/java/swarm_wars_library/ui/UI.java
@@ -120,11 +120,9 @@ public class UI {
         allStars.add(star3);
         allStars.add(star4);
         allStars.add(star5);
-
-
     }
 
-    void draw() {
+    void Draw() {
         background(backgroundimg);
 
         //draws twickly backround stars
@@ -170,118 +168,7 @@ public class UI {
 
 
 
-    public void mouseClicked() {
-        //LABEL SWAPS FOR STATES
-        //label1 swap - stars
-        if (mouseX >= positionx1- 10 && mouseX <=  positionx1+ 10 &&
-                mouseY >= positiony1- 10 && mouseY <= positiony1+10) {
-            if (swapLabel(label1) == false) {
-                star1.changecolor();
-            }
-            else {
-                checkForColourChanges();
-            }
 
-        }
-        //label3 swap - stars
-        else if (mouseX >= positionx3 - 10 && mouseX <=  positionx3+ 10 &&
-                mouseY >= positiony3 - 10 && mouseY <= positiony3 + 10) {
-            if (swapLabel(label3) == false) {
-                star3.changecolor();
-            }
-            else {
-                checkForColourChanges();
-            }
-
-        }
-        //swapping label2 - stars
-        else if (mouseX >= positionx2 - 10 && mouseX <=  positionx2 + 10 &&
-                mouseY >= positiony2 -10 && mouseY <= positionx2 + 10) {
-            if (swapLabel(label2) == false) {
-                star2.changecolor();
-            }
-            else {
-                checkForColourChanges();
-            }
-        }
-        //swapping label4 - stars
-        else if (mouseX >= positionx4 - 10 && mouseX <=  positionx4 + 10 &&
-                mouseY >= positiony4 - 10 && mouseY <= positiony4 + 10) {
-
-            if (swapLabel(label4) == false) {
-                star4.changecolor();
-            }
-            else {
-                checkForColourChanges();
-            }
-        }
-        //swapping for label5 - stars
-        else if (mouseX >= positionx5 - 10 && mouseX <=  positionx5 + 10 &&
-                mouseY >= positiony5 - 10  && mouseY <= positiony5 + 10) {
-
-            if (swapLabel(label5) == false) {
-                star5.changecolor();
-            }
-            else {
-                checkForColourChanges();
-            }
-        }
-
-
-        //LABEL SWAPS FOR TRANSITIONS
-        else if (mouseX >= (positionx1+positionx2)/2-30 &&
-                mouseX <= (positionx1+positionx2)/2-30+120 &&
-                mouseY >= (positiony1+positiony2)/2-27 &&
-                mouseY <= (positiony1+positiony2)/2-27+55) {
-            swapButton1();
-        }
-        else if (mouseX >= (positionx2+positionx5)/2-60 &&
-                mouseX <= (positionx2+positionx5)/2-60+120 &&
-                mouseY >= (positiony2+positiony5)/2-27 &&
-                mouseY <= (positiony2+positiony5)/2-27+55) {
-            swapButton2();
-        }
-        else if (mouseX >= (positionx4+positionx5)/2-60 &&
-                mouseX <= (positionx4+positionx5)/2-60 +120 &&
-                mouseY >= positiony5-27 &&
-                mouseY <= positiony5-27 +55) {
-            swapButton3();
-        }
-        else if (mouseX >= (positionx3+positionx4)/2-60 &&
-                mouseX <=  (positionx3+positionx4)/2-60+120 &&
-                mouseY >= (positiony3+positiony4)/2-27 &&
-                mouseY <= (positiony3+positiony4)/2-27 +55) {
-            swapButton4();
-        }
-        else if (mouseX >= (positionx3+positionx1)/2-90 &&
-                mouseX <=  (positionx3+positionx1)/2-90+120 &&
-                mouseY >= (positiony3+positiony1)/2-27 &&
-                mouseY <= (positiony3+positiony1)/2-27+55) {
-            swapButton5();
-        }
-
-        //START GAME
-        else if (mouseX >= 500 && mouseX <= 500+100 &&
-                mouseY >= 30 && mouseY <= 30+50) {
-            GameState = 2;
-            start.changecolor();
-        }
-
-        //TOOLTIP
-        else if (mouseX >= 50 - 20 && mouseX <= 50 +20 &&
-                mouseY >= 45 -20 && mouseY <= 45 + 20) {
-            if (showTooltip == false) {
-                showTooltip = true;
-                tooltip.changecolor();
-                info.changecolor();
-            }
-            else {
-                showTooltip = false;
-                tooltip.changecolor();
-                info.changecolor();
-            }
-        }
-    }
 
 
     public void checkForColourChanges() {
