44ef17247fbc428045968a8e7c1c6fc300946f23
antaires <11vdlo@gmail.com>
2019-05-20 12:36:39 +0100

    fixed color and placement of missile score text

--- src/main/java/swarm_wars_library/graphics/RenderPlayer2NumM.java
+++ src/main/java/swarm_wars_library/graphics/RenderPlayer2NumM.java
@@ -1,10 +1,10 @@
 package swarm_wars_library.graphics;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
 import swarm_wars_library.entities.Missile;
 
-import static processing.core.PConstants.LEFT;
-import static processing.core.PConstants.TOP;
+import static processing.core.PConstants.*;
 
 public class RenderPlayer2NumM {
     private int Mnum=0;
@@ -14,9 +14,10 @@ public class RenderPlayer2NumM {
     }
     public void update(int missilenum){
         this.Mnum=missilenum;
-        this.sketch.fill(255, 225, 32);
+        this.sketch.fill(255, 22, 65);
         this.sketch.textSize(30);
-        this.sketch.textAlign(LEFT, TOP);
-        this.sketch.text("Missile number: " + Mnum, 5, 155);
+        this.sketch.textAlign(PConstants.LEFT,PConstants.TOP);
+        this.sketch.text("Missiles: " + Mnum, this.sketch.width/2-150, 50);
     }
 }
+
